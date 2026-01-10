b3fe17ecd87655b3f43d42afa575538679b9dd8a
ukaushik-98 <kaushikuk101@gmail.com>
2026-01-10 14:58:46 -0800

    impl map in flatmap

--- src/main/scala/chapter6/Chapter6Generic.scala
+++ src/main/scala/chapter6/Chapter6Generic.scala
@@ -4,6 +4,12 @@ package chapter6
 
 case class State[S, +A](run: S => (A, S))
 
+object State:
+  def unit[S, A](a: A): State[S, A] =
+    State { s =>
+      (a, s)
+    }
+
 extension [S, A](underlying: State[S, A])
   def map[B](f: A => B): State[S, B] =
     State[S, B] { s =>
@@ -11,12 +17,17 @@ extension [S, A](underlying: State[S, A])
       (f(a), s2)
     }
 
+  def mapF[B](f: A => B): State[S, B] =
+    flatMap(a => State.unit(f(a)))
+
   def map2[B, C](action2: State[S, B])(f: (A, B) => C): State[S, C] =
     State[S, C] { s =>
       val (a, s2) = underlying.run(s)
       val (b, s3) = action2.run(s2)
       (f(a, b), s3)
     }
+
+  def map2F[B, C](action2: State[S, B])(f: (A, B) => C): State[S, C] = ???
 
   def flatMap[B](f: A => State[S, B]): State[S, B] =
     State { s =>
