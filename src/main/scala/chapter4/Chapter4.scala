122dd8b999be47d35d2e9f34cb11e677c631d460
ukaushik-98 <kaushikuk101@gmail.com>
2026-01-11 22:50:22 -0800

    impl with flatmap

--- src/main/scala/chapter4/Chapter4.scala
+++ src/main/scala/chapter4/Chapter4.scala
@@ -1,5 +1,7 @@
 package chapter4
 
+import chapter4.Option.unit
+
 enum Option[+A]:
   case Some(get: A)
   case None
@@ -9,33 +11,44 @@ enum Option[+A]:
       case Some(a) => Some(f(a))
       case None    => None
     }
+
+  def mapF[B](f: A => B): Option[B] = flatMap(a => unit(f(a)))
+
   def flatMap[B](f: A => Option[B]): Option[B] =
     this match {
       case Some(a) => f(a)
       case None    => None
     }
+
   def getOrElse[B >: A](default: => B): Option[B] =
     this match {
       case Some(a) => Some(a)
       case None    => Some(default)
     }
+
   def orElse[B >: A](ob: => Option[B]): Option[B] =
     this match {
       case Some(a) => Some(a)
       case None    => ob
     }
+
+  def orElseF[B >: A](ob: => Option[B]): Option[B] =
+    map(Some(_)).orElse(ob)
+
   def filter(f: A => Boolean): Option[A] =
     this match {
       case Some(a) => if f(a) then Some(a) else None
       case None    => None
     }
 
+  def filterF(f: A => Boolean): Option[A] =
+    flatMap(a => if f(a) then unit(a) else None)
+
 def mean(xs: Seq[Double]): Option[Double] =
   if xs.isEmpty then Option.None else Option.Some(xs.sum / xs.length)
 
 def variance(xs: Seq[Double]): Option[Double] =
   mean(xs).flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
-//  mean(xs) match {
-//    case Option.Some(m) => xs.map(x => math.pow(x - m, 2))
-//    case Option.None    => Option.None
-//  }
+
+object Option:
+  def unit[A](a: A): Option[A] = Some(a)
