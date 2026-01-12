1751030eceaa8903bd48b178b59ea858eaf30d3a
ukaushik-98 <kaushikuk101@gmail.com>
2026-01-11 21:43:37 -0800

    impl option monad

---
+++ src/main/scala/chapter4/Chapter4.scala
@@ -0,0 +1,31 @@
+package chapter4
+
+enum Option[+A]:
+  case Some(get: A)
+  case None
+
+  def map[B](f: A => B): Option[B] =
+    this match {
+      case Some(a) => Some(f(a))
+      case None    => None
+    }
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    this match {
+      case Some(a) => f(a)
+      case None    => None
+    }
+  def getOrElse[B >: A](default: => B): Option[B] =
+    this match {
+      case Some(a) => Some(a)
+      case None    => Some(default)
+    }
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this match {
+      case Some(a) => Some(a)
+      case None    => ob
+    }
+  def filter(f: A => Boolean): Option[A] =
+    this match {
+      case Some(a) => if f(a) then Some(a) else None
+      case None    => None
+    }
